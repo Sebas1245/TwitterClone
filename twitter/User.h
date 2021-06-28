95842dc4e3ce241d12c1875151087e34eec7e786
Sebastian Saldana Cardenas <sebastiansalc@fb.com>
2021-06-28 16:39:20 -0500

    Adds user and tweet models

---
+++ twitter/User.h
@@ -0,0 +1,16 @@
+//
+//  User.h
+//  Pods
+//
+//  Created by Sebastian Saldana Cardenas on 28/06/21.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface User : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ twitter/User.m
@@ -0,0 +1,12 @@
+//
+//  User.m
+//  Pods
+//
+//  Created by Sebastian Saldana Cardenas on 28/06/21.
+//
+
+#import "User.h"
+
+@implementation User
+
+@end
